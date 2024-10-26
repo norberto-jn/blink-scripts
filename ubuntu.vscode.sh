1720a39a709de306e00e47d7cd9bc482393f6f3b
Norberto Ferreira <norbertojunio@gmail.com>
2024-10-26 18:24:20 -0300

    feat: criado arquivo de instalação do VsCode

---
+++ ubuntu.vscode.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install software-properties-common apt-transport-https
+wget -qO - https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt update
+
+sudo apt install code
